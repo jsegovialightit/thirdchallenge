1d0487dc8921aaf54c8984198f424e676ad7c560
Joaquin Segovia Light-it <joaquin.segovia@lightit.io>
2022-10-17 10:16:33 -0300

    Docker env enables

--- src/ShowCommand.php
+++ src/ShowCommand.php
@@ -1,4 +1,6 @@
-<?php namespace Acme;
+<?php
+
+namespace Acme;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -8,7 +10,8 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Helper\TableSeparator;
 
-class ShowCommand extends Command {
+class ShowCommand extends Command
+{
 
     private $movieName;
     private $message;
@@ -26,20 +29,21 @@ class ShowCommand extends Command {
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $this->movieName = $input->getArgument('moviename');
-        $baseUrl = "http://www.omdbapi.com";//$_ENV["API_BASE_URL"];
+        //$baseUrl = "http://www.omdbapi.com";
+        $baseUrl = $_ENV["API_BASE_URL"];
         //$output->writeln("BaseURL from env: {$baseUrl}");
-        $apiKey = "e175c3c2";//$_ENV["API_KEY"];
+        $apiKey = "e175c3c2";
+        $apiKey = $_ENV["API_KEY"];
         //$output->writeln("ApiKey from env: {$apiKey}");
 
         $this->url = "{$baseUrl}/?apikey={$apiKey}&t={$this->movieName}";
         //$output->writeln("url: {$this->url}");
 
-        if (!$input->getOption('fullPlot'))
-        {
+        if (!$input->getOption('fullPlot')) {
             //$this->$url = 'http://www.omdbapi.com/?apikey=e175c3c2&t=scarface';
             $this->showInfo();
             $output->writeln("<info>******* Movie to show info:: {$this->movieName} >*******<info>");
-        }else{
+        } else {
             //$this->$url = 'http://www.omdbapi.com/?apikey=e175c3c2&t=scarface&plot=full';
             $this->url = $this->url . "&plot=full";
             $output->writeln("url: {$this->url}");
@@ -49,38 +53,38 @@ class ShowCommand extends Command {
         }
 
         $json = $this->response;
-        $array = json_decode($json,true);
+        $array = json_decode($json, true);
         $table = new Table($output);
         $table->setHeaderTitle("{$this->movieName}");
         foreach ($array as $clave => $valor) {
-            //$row = [ strval( $clave ), strval( $valor ) ];   
-            $row = [ $clave , strval( $valor ) ];   
-            $table->addRow($row);
+            if ($clave != "Ratings"){
+                //$row = [ strval( $clave ), strval( $valor ) ];   
+                $row = [$clave, strval($valor)];
+                $table->addRow($row);
+            }
         }
-        $table->render(); 
+        $table->render();
 
-        if (!$input->getOption('fullPlot'))
-        {
+        if (!$input->getOption('fullPlot')) {
             $output->writeln("\r\n\r\n<question> Want to know more about `{$this->movieName}`? Try adding --fullPlot after `show` command <question>");
         }
 
-       return Command::SUCCESS;
+        return Command::SUCCESS;
     }
 
     private function showInfo()
     {
-
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-        CURLOPT_URL => $this->url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_URL => $this->url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
         ));
 
         $this->response = curl_exec($curl);
@@ -92,5 +96,4 @@ class ShowCommand extends Command {
 
         //return $response;
     }
-
 }
